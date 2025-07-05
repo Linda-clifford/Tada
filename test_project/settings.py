1cda413fd7b7b9b61993a5f6cf8a6b70b50395d7
steve <gabrielstephen494@gmail.com>
2025-07-06 00:19:31 +0100

    fixed a common error

--- test_project/settings.py
+++ test_project/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     'default': {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # DATABASES = {
 #   'default': dj_database_url.parse("postgresql://postgres:SvTHigCCwfcfZXLlkZkmTMTwDNzJqnuZ@switchyard.proxy.rlwy.net:10937/railway")
