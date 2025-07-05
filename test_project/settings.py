0a9bb5e80a1ba2fedd40a5ae071308522d6ce5c0
steve <gabrielstephen494@gmail.com>
2025-07-06 00:59:14 +0100

    Changed setting.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!gx_0$86*jr$j-dxh++-jtoc5y14jm#6ukx5(7g$ns8pfy7r%('
-#SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-!gx_0$86*jr$j-dxh++-jtoc5y14jm#6ukx5(7g$ns8pfy7r%('
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -77,23 +77,16 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-# DATABASES = {
-#   'default': dj_database_url.parse("postgresql://postgres:SvTHigCCwfcfZXLlkZkmTMTwDNzJqnuZ@switchyard.proxy.rlwy.net:10937/railway")
-# }
-
-
 DATABASES = {
-    'defualt': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 AUTH_PASSWORD_VALIDATORS = [
