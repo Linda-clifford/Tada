292843c844236f23fa09adc6050d8b2ea56e9f19
steve <gabrielstephen494@gmail.com>
2025-07-08 14:50:12 +0100

    Enable WhiteNoise for static file serving

--- test_project/wsgi.py
+++ test_project/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
